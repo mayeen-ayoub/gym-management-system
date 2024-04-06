5261a87356fc1fe153292c37413e63ed3dee96c9
Romesa R <romesar@Romesas-MacBook-Pro.local>
2024-04-06 17:34:48 -0400

    Fix indentation

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,139 +1,139 @@
 CREATE TABLE Member (
-    id           SERIAL          PRIMARY KEY,
-    first_name   VARCHAR(255)    NOT NULL,
-    last_name    VARCHAR(255)    NOT NULL,
-    email        VARCHAR(255)    UNIQUE NOT NULL,
-    password     VARCHAR(255)    NOT NULL,
-    phone_number VARCHAR(15),
-    join_date    DATE            DEFAULT CURRENT_DATE
+  id           SERIAL          PRIMARY KEY,
+  first_name   VARCHAR(255)    NOT NULL,
+  last_name    VARCHAR(255)    NOT NULL,
+  email        VARCHAR(255)    UNIQUE NOT NULL,
+  password     VARCHAR(255)    NOT NULL,
+  phone_number VARCHAR(15),
+  join_date    DATE            DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE Trainer (
-    id          SERIAL          PRIMARY KEY,
-    first_name  VARCHAR(255)    NOT NULL,
-    last_name   VARCHAR(255)    NOT NULL,
-    email       VARCHAR(255)    UNIQUE NOT NULL,
-    password    VARCHAR(255)    NOT NULL
+  id          SERIAL          PRIMARY KEY,
+  first_name  VARCHAR(255)    NOT NULL,
+  last_name   VARCHAR(255)    NOT NULL,
+  email       VARCHAR(255)    UNIQUE NOT NULL,
+  password    VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE Admin (
-    id          SERIAL          PRIMARY KEY,
-    first_name  VARCHAR(255)    NOT NULL,
-    last_name   VARCHAR(255)    NOT NULL,
-    email       VARCHAR(255)    UNIQUE NOT NULL,
-    password    VARCHAR(255)    NOT NULL
+  id          SERIAL          PRIMARY KEY,
+  first_name  VARCHAR(255)    NOT NULL,
+  last_name   VARCHAR(255)    NOT NULL,
+  email       VARCHAR(255)    UNIQUE NOT NULL,
+  password    VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE Exercise_Routine (
-    id      SERIAL          PRIMARY KEY,
-    routine VARCHAR(255)    NOT NULL
+  id      SERIAL          PRIMARY KEY,
+  routine VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE Room_Booking (
-    id           SERIAL          PRIMARY KEY,
-    room_number  INT             NOT NULL,
-    event_type   VARCHAR(255)    NOT NULL,
-    date         DATE            NOT NULL,
-    start_time   TIME            NOT NULL,
-    end_time     TIME            NOT NULL
+  id           SERIAL          PRIMARY KEY,
+  room_number  INT             NOT NULL,
+  event_type   VARCHAR(255)    NOT NULL,
+  date         DATE            NOT NULL,
+  start_time   TIME            NOT NULL,
+  end_time     TIME            NOT NULL
 );
 
 CREATE TABLE Equipment (
-    id                   SERIAL      PRIMARY KEY,
-    needs_maintenance    BOOLEAN     NOT NULL,
-    last_maintained      DATE        NOT NULL
+  id                   SERIAL      PRIMARY KEY,
+  needs_maintenance    BOOLEAN     NOT NULL,
+  last_maintained      DATE        NOT NULL
 );
 
 CREATE TABLE Fitness_Goal (
-    id               SERIAL          PRIMARY KEY,
-    member_id        INT,
-    target_weight    INT,
-    target_time      NUMERIC(3, 1),
-    target_calories  INT,
-    FOREIGN KEY (member_id) 
-        REFERENCES Member (id)
+  id               SERIAL          PRIMARY KEY,
+  member_id        INT,
+  target_weight    INT,
+  target_time      NUMERIC(3, 1),
+  target_calories  INT,
+  FOREIGN KEY (member_id) 
+    REFERENCES Member (id)
 );
 
 CREATE TABLE Health_Metrics (
-    id                  SERIAL          PRIMARY KEY,
-    member_id           INT,
-    weight              INT,
-    heart_rate          INT,
-    calories_burned     INT,
-    time_spent_at_gym   NUMERIC(3, 1),
-    date                DATE            DEFAULT CURRENT_DATE,
-    FOREIGN KEY (member_id) 
-        REFERENCES Member (id)
+  id                  SERIAL          PRIMARY KEY,
+  member_id           INT,
+  weight              INT,
+  heart_rate          INT,
+  calories_burned     INT,
+  time_spent_at_gym   NUMERIC(3, 1),
+  date                DATE            DEFAULT CURRENT_DATE,
+  FOREIGN KEY (member_id) 
+    REFERENCES Member (id)
 );
 
 CREATE TABLE Bill (
-    id              SERIAL          PRIMARY KEY,
-    member_id       INT,
-    amount          NUMERIC(5, 2)   NOT NULL,
-    fee_type        VARCHAR(255)    NOT NULL,
-    invoice_date    DATE            NOT NULL,
-    payment_date    DATE,
-    FOREIGN KEY (member_id) 
-        REFERENCES Member (id)
+  id              SERIAL          PRIMARY KEY,
+  member_id       INT,
+  amount          NUMERIC(5, 2)   NOT NULL,
+  fee_type        VARCHAR(255)    NOT NULL,
+  invoice_date    DATE            NOT NULL,
+  payment_date    DATE,
+  FOREIGN KEY (member_id) 
+    REFERENCES Member (id)
 );
 
 CREATE TABLE Availability (
-    id              SERIAL      PRIMARY KEY,
-    trainer_id      INT,
-    day_of_week     VARCHAR(9)  NOT NULL,
-    start_time      TIME        NOT NULL,
-    end_time        TIME        NOT NULL,
-    FOREIGN KEY (trainer_id) 
-        REFERENCES Trainer (id)
+  id              SERIAL      PRIMARY KEY,
+  trainer_id      INT,
+  day_of_week     VARCHAR(9)  NOT NULL,
+  start_time      TIME        NOT NULL,
+  end_time        TIME        NOT NULL,
+  FOREIGN KEY (trainer_id) 
+    REFERENCES Trainer (id)
 );
 
 CREATE TABLE Personal_Session (
-    id           SERIAL  PRIMARY KEY,
-    member_id    INT,
-    trainer_id   INT,
-    date         DATE    NOT NULL,
-    start_time   TIME    NOT NULL,
-    end_time     TIME    NOT NULL,
-    FOREIGN KEY (member_id) 
-        REFERENCES Member (id),
-    FOREIGN KEY (trainer_id) 
-        REFERENCES Trainer (id)
+  id           SERIAL  PRIMARY KEY,
+  member_id    INT,
+  trainer_id   INT,
+  date         DATE    NOT NULL,
+  start_time   TIME    NOT NULL,
+  end_time     TIME    NOT NULL,
+  FOREIGN KEY (member_id) 
+    REFERENCES Member (id),
+  FOREIGN KEY (trainer_id) 
+    REFERENCES Trainer (id)
 );
 
 CREATE TABLE Personal_Session_Exercise_Routine (
-    personal_session_id INT,
-    exercise_routine_id INT,
-    FOREIGN KEY (personal_session_id) 
-        REFERENCES Personal_Session (id),
-    FOREIGN KEY (exercise_routine_id) 
-        REFERENCES Exercise_Routine (id)
+  personal_session_id INT,
+  exercise_routine_id INT,
+  FOREIGN KEY (personal_session_id) 
+    REFERENCES Personal_Session (id),
+  FOREIGN KEY (exercise_routine_id) 
+    REFERENCES Exercise_Routine (id)
 );
 
 CREATE TABLE Group_Session (
-    id                SERIAL          PRIMARY KEY,
-    room_booking_id   INT,
-    trainer_id        INT,
-    title             VARCHAR(255)    NOT NULL,
-    FOREIGN KEY (room_booking_id) 
-        REFERENCES Room_Booking (id),
-    FOREIGN KEY (trainer_id) 
-        REFERENCES Trainer (id)
+  id                SERIAL          PRIMARY KEY,
+  room_booking_id   INT,
+  trainer_id        INT,
+  title             VARCHAR(255)    NOT NULL,
+  FOREIGN KEY (room_booking_id) 
+    REFERENCES Room_Booking (id),
+  FOREIGN KEY (trainer_id) 
+    REFERENCES Trainer (id)
 );
 
 CREATE TABLE Group_Session_Exercise_Routine (
-    group_session_id      INT,
-    exercise_routine_id   INT,
-    FOREIGN KEY (group_session_id) 
-        REFERENCES Group_Session (id),
-    FOREIGN KEY (exercise_routine_id) 
-        REFERENCES Exercise_Routine (id)
+  group_session_id      INT,
+  exercise_routine_id   INT,
+  FOREIGN KEY (group_session_id) 
+    REFERENCES Group_Session (id),
+  FOREIGN KEY (exercise_routine_id) 
+    REFERENCES Exercise_Routine (id)
 );
 
 CREATE TABLE Member_Group_Session (
-    member_id        INT,
-    group_session_id  INT,
-    FOREIGN KEY (member_id) 
-        REFERENCES Member (id),
-    FOREIGN KEY (group_session_id) 
-        REFERENCES Group_Session (id)
+  member_id        INT,
+  group_session_id  INT,
+  FOREIGN KEY (member_id) 
+    REFERENCES Member (id),
+  FOREIGN KEY (group_session_id) 
+    REFERENCES Group_Session (id)
 );
--- member.js
+++ member.js
@@ -5,71 +5,71 @@ class Member {
     this.client = client;
   }
 
-    async checkIfMember() {
-        try {
-            const email = prompt("Enter member email: ");
-            const password = prompt("Enter password: ");
-            const dbPasswordResult = await this.client.query('SELECT id, password FROM Member WHERE email = $1;', [email]);
-            const dbPassword = dbPasswordResult?.rows[0]?.password;
+  async checkIfMember() {
+    try {
+      const email = prompt("Enter member email: ");
+      const password = prompt("Enter password: ");
+      const dbPasswordResult = await this.client.query('SELECT id, password FROM Member WHERE email = $1;', [email]);
+      const dbPassword = dbPasswordResult?.rows[0]?.password;
 
-            if (dbPasswordResult?.rowCount === 0 || password !== dbPassword) {
-                console.log(`Incorrect password. Terminating...`);
-                return null;
-            }
-            return dbPasswordResult.rows[0].id;
-        } catch(error) {
-            console.log(`ERROR: ${error.message}\n`);
-            return null;
-        }
+      if (dbPasswordResult?.rowCount === 0 || password !== dbPassword) {
+        console.log(`Incorrect password. Terminating...`);
+        return null;
+      }
+      return dbPasswordResult.rows[0].id;
+    } catch(error) {
+      console.log(`ERROR: ${error.message}\n`);
+      return null;
     }
+  }
 
-    async register() {
-        try {
-            const firstName = prompt("Enter your first name: ");
-            const lastName = prompt("Enter your last name: ");
-            const email = prompt("Enter your email: ");
-            const password = prompt("Enter your password: ");
-            let phoneNumber = null;
-            const wantsPhoneNumber = prompt("Would you like to associate a phone number with your profile? Y/N: ").toLowerCase();
-            if (wantsPhoneNumber === "y") {
-                phoneNumber = prompt("Enter your phone number: ");
-            }
+  async register() {
+    try {
+      const firstName = prompt("Enter your first name: ");
+      const lastName = prompt("Enter your last name: ");
+      const email = prompt("Enter your email: ");
+      const password = prompt("Enter your password: ");
+      let phoneNumber = null;
+      const wantsPhoneNumber = prompt("Would you like to associate a phone number with your profile? Y/N: ").toLowerCase();
+      if (wantsPhoneNumber === "y") {
+        phoneNumber = prompt("Enter your phone number: ");
+      }
 
-            const insertMemberQuery = `
-                INSERT INTO Member (first_name, last_name, email, password, phone_number) VALUES ($1, $2, $3, $4, $5) RETURNING id;
-            `;
+      const insertMemberQuery = `
+        INSERT INTO Member (first_name, last_name, email, password, phone_number) VALUES ($1, $2, $3, $4, $5) RETURNING id;
+      `;
 
-            const result = await this.client.query(insertMemberQuery, [firstName, lastName, email, password, phoneNumber]);
-            const memberId = result.rows[0].id;
+      const result = await this.client.query(insertMemberQuery, [firstName, lastName, email, password, phoneNumber]);
+      const memberId = result.rows[0].id;
 
-            console.log(`Welcome ${firstName}! It's now time to set your fitness goals.`);
-            let targetWeight = null;
-            let targetTime = null;
-            let targetCalories = null;
-            const wantsTargetWeight = prompt("Would you like to set a target weight? Y/N: ").toLowerCase();
-            if (wantsTargetWeight === "y") {
-                targetWeight = parseInt(prompt("Enter your target weight (in pounds): "));
-            }
-            const wantsTargetTime = prompt("Would you like to set a target time to spend at the gym each week? Y/N: ").toLowerCase();
-            if (wantsTargetTime === "y") {
-                targetTime = parseFloat(prompt("Enter your target time (in hours): "));
-            }
-            const wantsTargetCalories = prompt("Would you like to set a target number of calories to burn each week? Y/N: ").toLowerCase();
-            if (wantsTargetCalories === "y") {
-                targetCalories = parseInt(prompt("Enter your target calories: "));
-            }
+      console.log(`Welcome ${firstName}! It's now time to set your fitness goals.`);
+      let targetWeight = null;
+      let targetTime = null;
+      let targetCalories = null;
+      const wantsTargetWeight = prompt("Would you like to set a target weight? Y/N: ").toLowerCase();
+      if (wantsTargetWeight === "y") {
+        targetWeight = parseInt(prompt("Enter your target weight (in pounds): "));
+      }
+      const wantsTargetTime = prompt("Would you like to set a target time to spend at the gym each week? Y/N: ").toLowerCase();
+      if (wantsTargetTime === "y") {
+        targetTime = parseFloat(prompt("Enter your target time (in hours): "));
+      }
+      const wantsTargetCalories = prompt("Would you like to set a target number of calories to burn each week? Y/N: ").toLowerCase();
+      if (wantsTargetCalories === "y") {
+        targetCalories = parseInt(prompt("Enter your target calories: "));
+      }
 
-            const insertFitnessGoalQuery = `
-                INSERT INTO Fitness_Goal (member_id, target_weight, target_time, target_calories) VALUES ($1, $2, $3, $4);
-            `;
+      const insertFitnessGoalQuery = `
+        INSERT INTO Fitness_Goal (member_id, target_weight, target_time, target_calories) VALUES ($1, $2, $3, $4);
+      `;
 
-            await this.client.query(insertFitnessGoalQuery, [memberId, targetWeight, targetTime, targetCalories]);
+      await this.client.query(insertFitnessGoalQuery, [memberId, targetWeight, targetTime, targetCalories]);
 
-            console.log("Thanks for registering! Your information and fitness goals have been saved.");
-        } catch(error) {
-            console.log(`ERROR: ${error.message}\n`);
-            return;
-        }
+      console.log("Thanks for registering! Your information and fitness goals have been saved.");
+    } catch(error) {
+      console.log(`ERROR: ${error.message}\n`);
+      return;
     }
+  }
 }
 module.exports = Member;
